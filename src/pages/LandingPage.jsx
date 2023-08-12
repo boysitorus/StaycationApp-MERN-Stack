d3e7451005d271de0d3ca1df5bb3c45edde4750f
boyysitorus <sitorusboy0123@gmail.com>
2023-08-12 19:18:19 +0700

    Section Hero, MostPick, and Categories

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,13 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 import Header from "../parts/Header";
+import Hero from "../parts/Hero";
+import landingPage from "../json/landingPage.json";
+import MostPicked from "../parts/MostPicked";
+import Category from '../parts/Category';
 
-export default class LandingPage extends Component{
-    render() {
-        return(
-            <>
-                <Header ></Header>
-            </>
-        )
-    }
+export default class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
+
+  render() {
+    return (
+      <>
+        <Header></Header>
+        <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
+        <MostPicked ref={this.refMostPicked} data={landingPage.mostPicked} />
+        <Category data={landingPage.categories}></Category>
+      </>
+    );
+  }
 }
