388bd35404ccbdd37860d9f548fe5bb09127d3fe
boyysitorus <sitorusboy0123@gmail.com>
2023-09-08 14:40:20 +0700

    testi and footer

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,6 +5,8 @@ import Hero from "../parts/Hero";
 import landingPage from "../json/landingPage.json";
 import MostPicked from "../parts/MostPicked";
 import Category from '../parts/Category';
+import Testimony from "../parts/Testimony";
+import Footer from "../parts/Footer";
 
 export default class LandingPage extends Component {
   constructor(props) {
@@ -18,7 +20,9 @@ export default class LandingPage extends Component {
         <Header></Header>
         <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
         <MostPicked ref={this.refMostPicked} data={landingPage.mostPicked} />
-        <Category data={landingPage.categories}></Category>
+        <Category data={landingPage.categories}></Category> 
+        <Testimony data={landingPage.testimonial}></Testimony>
+        <Footer></Footer>
       </>
     );
   }
